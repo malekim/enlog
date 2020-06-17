41f19a16a499fbaf3da558551b6f0d01fed4fb82
Łukasz Małek <dev@malek.im>
2020-06-17 18:57:57 +0200

    Add unit tests

---
+++ enlog_test.go
@@ -0,0 +1,70 @@
+package enlog
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"reflect"
+	"testing"
+
+	"bou.ke/monkey"
+)
+
+func TestNew(t *testing.T) {
+	var l Enlogger
+	l = New()
+
+	if l == nil {
+		t.Fatal("Error opening file")
+	}
+
+	lType := reflect.TypeOf(l)
+
+	if fmt.Sprintf("%T", l) != "*enlog.Enlog" {
+		t.Fatal("Expected", "*enlog.Enlog", "Got", lType)
+	}
+
+	testRemoveFiles(t, []string{"info", "debug", "error", "trace"})
+}
+
+func TestNewError(t *testing.T) {
+	// force an error
+	// var guard *monkey.PatchGuard
+	// note that during test there must be flag -gcflags=-l
+	patch := monkey.Patch(os.OpenFile, func(name string, flag int, perm os.FileMode) (*os.File, error) {
+		return nil, errors.New("Forced error")
+	})
+	defer patch.Unpatch()
+	en := New()
+	if en.InfoLog != nil {
+		t.Error("InfoLog should be nil here")
+	}
+	if en.DebugLog != nil {
+		t.Error("DebugLog should be nil here")
+	}
+	if en.ErrorLog != nil {
+		t.Error("ErrorLog should be nil here")
+	}
+	if en.TraceLog != nil {
+		t.Error("TraceLog should be nil here")
+	}
+}
+
+func testFile(t *testing.T, category string) {
+	f, err := os.Stat(fmt.Sprintf("%s.log", category))
+	if err != nil {
+		t.Fatalf("%s.log file does not exists", category)
+	}
+	if 0 >= f.Size() {
+		t.Fatalf("%s.log file is empty", category)
+	}
+}
+
+func testRemoveFiles(t *testing.T, categories []string) {
+	for i := len(logTypes) - 1; i >= 0; i-- {
+		err := os.Remove(fmt.Sprintf("%s.log", categories[i]))
+		if err != nil {
+			t.Fatalf("%s.log could not be deleted", categories[i])
+		}
+	}
+}
---
+++ trace_test.go
@@ -0,0 +1,59 @@
+package enlog
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTrace(t *testing.T) {
+	var l Enlogger = New()
+	var msg string
+	var ltype string
+	l.SetAfterLogEvent(func(logType string, message string) {
+		ltype = logType
+		msg = message
+	})
+	testMsg := "trace1"
+	l.Trace(testMsg)
+	if msg != testMsg {
+		t.Error("Expected", msg, "got", testMsg)
+	}
+	if ltype != "trace" {
+		t.Error("Expected", "trace", "got", ltype)
+	}
+
+	testFile(t, "trace")
+	testRemoveFiles(t, []string{"info", "debug", "error", "trace"})
+}
+
+func TestTracef(t *testing.T) {
+	var l Enlogger = New()
+	var msg string
+	var ltype string
+	l.SetAfterLogEvent(func(logType string, message string) {
+		ltype = logType
+		msg = message
+	})
+	testMsg := "trace"
+	l.Tracef(testMsg)
+	if msg != testMsg {
+		t.Error("Expected", msg, "got", testMsg)
+	}
+	if ltype != "trace" {
+		t.Error("Expected", "trace", "got", ltype)
+	}
+	argf1 := 15
+	argf2 := "train"
+	testMsg = "trace %d trace %s"
+	testMsgf := fmt.Sprintf(testMsg, argf1, argf2)
+	l.Tracef(testMsg, argf1, argf2)
+	if testMsgf != msg {
+		t.Error("Expected", msg, "got", testMsgf)
+	}
+	if ltype != "trace" {
+		t.Error("Expected", "trace", "got", ltype)
+	}
+
+	testFile(t, "trace")
+	testRemoveFiles(t, []string{"info", "debug", "error", "trace"})
+}
