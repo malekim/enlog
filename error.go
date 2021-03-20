0c425a4d3817e938baff689878b876d92b7f8263
Łukasz Małek <dev@malek.im>
2021-03-20 14:24:53 +0100

    Update package comments

--- error.go
+++ error.go
@@ -2,7 +2,9 @@ package enlog
 
 import "fmt"
 
-// Error log
+// Error is a method that logs messages
+// to ErrorLog. By default messages are shown
+// in red color.
 func (l *Enlog) Error(log string) {
 	// turn on color
 	fmt.Printf("\033[1;%dm", l.ErrorLog.color)
@@ -12,7 +14,9 @@ func (l *Enlog) Error(log string) {
 	l.AfterLog("error", log)
 }
 
-// Errorf log
+// Errorf is a method that logs messages
+// to ErrorLog. By default messages are shown
+// in red color.
 func (l *Enlog) Errorf(log string, args ...interface{}) {
 	l.Error(fmt.Sprintf(log, args...))
 }
